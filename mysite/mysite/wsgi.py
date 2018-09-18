9a44e6b9ec2eb688424edeb75f67b8c83f3cb392
jayanth0906 <jayanthtirupathi12@gmail.com>
2018-09-18 17:19:00 -0500

    Adding Files to Repo

---
+++ mysite/mysite/wsgi.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for mysite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
+import whitenoise.django
+from whitenoise import WhiteNoise
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+application = get_wsgi_application()
+application = WhiteNoise(application)
